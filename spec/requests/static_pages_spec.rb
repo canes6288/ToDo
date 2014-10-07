880812aa2e36b2a09e37f748bf0d866a2ba48432
canes6288 <joshkushner1@gmail.com>
2014-10-07 11:26:05 -0400

    Running some new tests on To Do app

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+describe "static_pages" do
+
+  subject { page }
+
+  describe "home page" do
+    # 1. Visit the home page
+    before { visit home_path }
+    # 2. Test that it should have a h1 tag of "Welcome"
+    it { should have_selector('h1', "Welcome") }
+    # 3. Test that it should have a title of "Todo | Home"
+    it { should have_title("Todo | Home") }
+end
+
+  describe "about" do
+    before { visit about_path }
+    # it should have a h1 tag of "About Me"
+    it { should have_selector('h1', "About Me") }
+    # it should have a title of "Todo | About"
+    it { should have_title("Todo | About")}
+  end
+
+  describe "help" do
+    before {visit help_path}
+    # it should have a h1 tag of "Help / FAQ"
+    it { should have_selector('h1', "Help / FAQ") }
+    # it should have a title of "Todo | Help"
+    it { should have_title("Todo | Help")}
+  end
+end
